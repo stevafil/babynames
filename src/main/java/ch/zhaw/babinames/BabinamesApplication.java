8d80f5695168ef70d7feee6bcca7a20a7b835ac3
stevafil <stevafil@students.zhaw.ch>
2023-09-29 19:01:50 +0200

    first commit

---
+++ src/main/java/ch/zhaw/babinames/BabinamesApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.babinames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabinamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabinamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/babinames/BabinamesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.babinames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabinamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
