bdb67ce374234a5b6f57f4e5fadf332bc8c085ca
stevafil <stevafil@students.zhaw.ch>
2023-10-03 14:23:18 +0200

    #3 issue

--- src/main/java/ch/zhaw/babinames/model/Name.java
+++ src/main/java/ch/zhaw/babinames/model/Name.java
@@ -1,30 +1,12 @@
 package ch.zhaw.babinames.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+// TODO: Annotationen für Getter, Konstruktor und toString einfügen
 public class Name {
     private String name;
     private String geschlecht;
     private int anzahl;
-
-    public Name(String name, String geschlecht, int anzahl) {
-        this.name = name;
-        this.geschlecht = geschlecht;
-        this.anzahl = anzahl;
-    }
-
-    public String getGeschlecht() {
-        return geschlecht;
-    }
-
-    public int getAnzahl() {
-        return anzahl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
-    }
 }
